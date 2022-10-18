64733b6cbdb1db068a76c98edbdac3cb43341a98
sriputrianti <putrimateka@gmail.com>
2022-10-18 19:08:43 +0800

    Initial commit

---
+++ app/src/main/java/com/f55121055/percobaanmobile2/TugasPutri.java
@@ -0,0 +1,14 @@
+package com.f55121055.percobaanmobile2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TugasPutri extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tugas_putri);
+    }
+}
